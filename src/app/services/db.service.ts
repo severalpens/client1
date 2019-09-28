be9badf79e081054b459591be363e29c91b7784c
Paul C <paulcollinsaf@gmail.com>
2019-09-28 20:14:58 +1000

    master

--- src/app/services/db.service.ts
+++ src/app/services/db.service.ts
@@ -1,7 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable, of, from, interval } from 'rxjs'; // use obs$ naming convention
+import { ajax } from 'rxjs/ajax';
 import { catchError, map, tap } from 'rxjs/operators';
+
+// Create an Observable out of a promise
+const data = from(fetch('/api/endpoint'));
+// Subscribe to begin listening for async result
+data.subscribe({
+  next(response) { console.log(response); },
+  error(err) { console.error('Error: ' + err); },
+  complete() { console.log('Completed'); }
+});
+
+// Create an Observable that will create an AJAX request
+const apiData = ajax('/api/data');
+// Subscribe to create the request
+apiData.subscribe(res => console.log(res.status, res.response));
+
+
 
 @Injectable({
   providedIn: 'root'

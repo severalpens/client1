d13739c97109ebe29ef591aef0f7746fc65465d4
Paul C <paulcollinsaf@gmail.com>
2019-09-28 23:59:56 +1000

    master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './components/index/index.component';
+import { LoginComponent } from './components/login/login.component';
+import { AccountComponent } from './components/account/account.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'index', component: IndexComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'account', component: AccountComponent }]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

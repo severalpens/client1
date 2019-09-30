a8e8def9f58b62c816a98e37235e4daa21b0fe1c
Paul C <paulcollinsaf@gmail.com>
2019-09-30 10:42:14 +1000

    develop: rebuild

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { IndexComponent } from './components/index/index.component';
 import { LoginComponent } from './components/login/login.component';
 import { AccountComponent } from './components/account/account.component';
 import { RegisterComponent } from './components/register/register.component';
 
 
 const routes: Routes = [
-  { path: 'index', component: IndexComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'account', component: AccountComponent }]
--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SocketService } from '../../services/socket.service';
 
 
+
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
